257a13cf04b3f0c31b9387c87cd35a39321d318b
webdevjohn <pye.john@gmail.com>
2021-03-09 16:59:45 +0000

    user database migration fix

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->boolean('banned', false);
+            $table->boolean('banned')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
