3c1fab6823717816576982c4afbdebb957219222
webdevjohn <pye.john@gmail.com>
2021-03-19 22:12:12 +0000

    refactored AlbumsController

--- app/Http/Controllers/AlbumsController.php
+++ app/Http/Controllers/AlbumsController.php
@@ -2,22 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use App\Repositories\AlbumRepository;
+use App\Models\Album;
 
 class AlbumsController extends Controller
 {
     public function __construct(
-        protected AlbumRepository $albums
+        protected Album $albums
     ){}
 
     /**
      * Display a paginated list of Albums
-     *           
+     *
+     * @return Illuminate\View\View 
      */
     public function index()
     {
         return View('albums.index', array(
-            'albums' => $this->albums->getAlbums()   
+            'albums' => $this->albums->orderBy('purchase_date', 'DESC')->paginate(48)	  
         ));
     }
 }
