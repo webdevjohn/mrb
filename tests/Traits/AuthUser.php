648555ccd086d814773ff18298ed28ea8573749c
webdevjohn <pye.john@gmail.com>
2021-03-28 16:14:30 +0100

    added setup method to test classes

--- tests/Traits/AuthUser.php
+++ tests/Traits/AuthUser.php
@@ -4,13 +4,14 @@ namespace Tests\Traits;
 
 use App\Models\Role;
 use App\Models\User;
+use Illuminate\Support\Str;
 
 trait AuthUser {
 
     function createAuthUser(string $role = 'Admin'): void
     {
         User::factory()->createOne()->roles()->attach(
-            Role::factory()->createOne(['role' => $role])
+            Role::factory()->createOne(['role' => Str::title($role)])
         );
 
         $this->actingAs(
