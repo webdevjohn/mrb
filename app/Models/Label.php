ecfc5beb536fc5dd0cf7eaefb43b930850611708
webdevjohn <pye.john@gmail.com>
2021-04-28 14:01:44 +0100

    removed Label model observers

--- app/Models/Label.php
+++ app/Models/Label.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Str;
 
 class Label extends Model
 {
