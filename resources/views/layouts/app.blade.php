463c39473a93783cbb983ffb0f96189406e07734
webdevjohn <pye.john@gmail.com>
2021-05-13 18:28:26 +0100

    removed default-app-bar.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,32 @@
     <body>
         <div id="colour-strip"></div>
         
-        @include('_partials.default-app-bar') 
+        <section id="app-bar"> 
+            <div class="wrapper">
+                <div id="logo-con">
+                    <a href="{{ route('homepage') }}" id="logo">MyRecordBox</a>                
+                </div>
+ 
+                <section id="main-nav">
+                    <nav>
+                        <ul>
+                            <li><a href="{{ route('artists.index') }}">Artists</a></li>  
+                            <li><a href="{{ route('genres.index') }}">Genres</a></li>  
+                            <li><a href="{{ route('labels.index') }}">Labels</a></li>     
+                            <li><a href="{{ route('albums.index') }}">Albums</a></li>     
+                            <li><a href="{{ route('playlists.index') }}">Playlists</a></li>    
+                            @if (Auth::check())
+                            <li>            
+                                <a href="{{ route('dashboard') }}">Your Dashboard</a>                      
+                            </li>
+                            @endif   
+                        </ul>               
+                    </nav>             
+                </section>
+            </div>
+            <br class="clear" />
+        </section>
+
         {{ $slot }}
 
          <footer id="page-footer">
